75850e35f3847dde04139f33f481c8b3caeaf601
unknown <heetmehta208@gmail.com>
2024-10-16 19:22:28 +0530

    Fix bcrypt issue

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const app = express()
 require('dotenv').config();
